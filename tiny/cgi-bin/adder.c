51763e843bbcf1acd6dcd86bb6c891e1d18ef333
tjvm0877 <itsmine096@gmail.com>
2025-11-04 23:34:23 +0900

    Add tiny server handling for dynamic and static content

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,10 +1,19 @@
-/*
- * adder.c - a minimal CGI program that adds two numbers together
- */
-/* $begin adder */
+/* adder.c - a minimal CGI program that adds two numbers together */
+#include <stdlib.h>
 #include "csapp.h"
 
-int main(void) {
-  exit(0);
+int main(void)
+{
+    const char *qs = getenv("QUERY_STRING");
+    int a = 0, b = 0;
+
+    printf("Content-type: text/html\r\n\r\n");
+    if (!qs || sscanf(qs, "%d&%d", &a, &b) != 2)
+    {
+        printf("<html><body>Invalid query</body></html>\n");
+        exit(0);
+    }
+
+    printf("<html><body>sum=%d</body></html>\n", a + b);
+    exit(0);
 }
-/* $end adder */
